8496f31a036331ebf537af20877bb3d275358557
Nikola <antic.nikola@outlook.com>
2016-11-02 13:33:36 +0100

    Initial movement and animation implemented

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import rafgfxlib.GameFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Igrica kida!");
+		//System.out.println("Igrica kida!");
+		GameFrame gf = new SideScroller("Kid A - The Game", 800, 600);
+		gf.initGameWindow();
 
 	}
 
